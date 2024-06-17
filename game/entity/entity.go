cfa0186a2cb5439fb31f5a59f30fe015df7ce75f
Sebastian Jung <jung@denic.de>
2024-06-17 21:59:10 +0200

    Add movement logic to enemy entity

--- game/entity/entity.go
+++ game/entity/entity.go
@@ -5,7 +5,8 @@ import "tinygo.org/x/drivers/pixel"
 // EnemyEntity extends the Entity by a collision flag.
 type EnemyEntity struct {
 	*Entity
-	DidCollide bool
+	DidCollide    bool
+	HasBeenScored bool
 }
 
 // Entity represents a game entity. Usable for dynamic and static objects.
@@ -17,7 +18,7 @@ type Entity struct {
 	Image  pixel.Image[pixel.RGB565BE]
 }
 
-// Entity HasCollision checks
+// HasCollision checks for collisions between entities based on position and size
 func (e *Entity) HasCollision(e2 *Entity) bool {
 	if e.PosX+e.Width < e2.PosX {
 		return false
@@ -32,4 +33,17 @@ func (e *Entity) HasCollision(e2 *Entity) bool {
 	}
 
 	return true
+}
+
+// Move pushes the enemy towards the left, based on the time which has passed since the last update and the current speed of the enemy
+func (e *EnemyEntity) Move(deltaTime, movementSpeed float32) {
+	e.Entity.PosX = e.Entity.PosX - movementSpeed*deltaTime
+}
+
+func (e *Entity) ShouldBeCulled() bool {
+	return e.PosX+e.Width <= 0
+}
+
+func (e *EnemyEntity) HasBeenPassedByPlayer(player *Entity) bool {
+	return player.PosX > e.PosX+e.Width
 }
--- game/entity/entity_test.go
+++ game/entity/entity_test.go
@@ -1,9 +1,9 @@
 package entity_test
 
 import (
-	"testing"
-
 	"github.com/conejoninja/gopherbadge/game/entity"
+	"math"
+	"testing"
 )
 
 func TestHasCollision(t *testing.T) {
@@ -100,6 +100,90 @@ func TestHasCollision(t *testing.T) {
 			collision := tc.player.HasCollision(tc.enemy)
 			if collision != tc.shouldCollide {
 				t.Errorf("expected %v, got %v", tc.shouldCollide, collision)
+			}
+		})
+	}
+}
+
+func TestMove(t *testing.T) {
+	tt := []struct {
+		description       string
+		enemies           []*entity.EnemyEntity
+		timeDelta         float32
+		enemySpeeds       []float32
+		expectedPositions []float32
+	}{
+		{
+			description: "enemies don't move if no time passes",
+			enemies: []*entity.EnemyEntity{
+				{
+					Entity: &entity.Entity{
+						PosX: 0,
+					},
+				},
+				{
+					Entity: &entity.Entity{
+						PosX: 100,
+					},
+				},
+			},
+			timeDelta:         0,
+			enemySpeeds:       []float32{12, 24},
+			expectedPositions: []float32{0, 100},
+		},
+		{
+			description: "enemies with a speed of 0 never move",
+			enemies: []*entity.EnemyEntity{
+				{
+					Entity: &entity.Entity{
+						PosX: 0,
+					},
+				},
+			},
+			timeDelta:         math.MaxFloat32,
+			enemySpeeds:       []float32{0},
+			expectedPositions: []float32{0},
+		},
+		{
+			description: "enemies with a speed greater 0 move for in a time greater 0",
+			enemies: []*entity.EnemyEntity{
+				{
+					Entity: &entity.Entity{
+						PosX: 10,
+					},
+				},
+				{
+					Entity: &entity.Entity{
+						PosX: 100,
+					},
+				},
+			},
+			timeDelta:         0.5,
+			enemySpeeds:       []float32{10, 100},
+			expectedPositions: []float32{5, 50},
+		},
+		{
+			description: "enemies can move off screen (into the negative posX)",
+			enemies: []*entity.EnemyEntity{
+				{
+					Entity: &entity.Entity{
+						PosX: 10,
+					},
+				},
+			},
+			timeDelta:         0.5,
+			enemySpeeds:       []float32{40},
+			expectedPositions: []float32{-10},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.description, func(t *testing.T) {
+			for idx := range tc.enemies {
+				tc.enemies[idx].Move(tc.timeDelta, tc.enemySpeeds[idx])
+				if tc.enemies[idx].PosX != tc.expectedPositions[idx] {
+					t.Errorf("expected enemy at position %f, got %f", tc.expectedPositions[idx], tc.enemies[idx].PosX)
+				}
 			}
 		})
 	}
